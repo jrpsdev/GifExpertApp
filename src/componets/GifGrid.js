34f84eb47bfd603f9989c1aacf2cfe5fd88b5d67
jrpsdev <jrpsdev@gmail.com>
2021-06-28 13:33:03 -0500

    Solucion Api Fetch

--- src/componets/GifGrid.js
+++ src/componets/GifGrid.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 export const GifGrid = ( { category } ) => {
 
-    const getGifs = () => {
+    const getGifs = async () => {
+        const URL = 'https://api.giphy.com/v1/gifs/search?q=Samurai+X&limit=5&api_key=xihgKvxIn5LiQz36dS5z6Fn3p3yXMPKM';
+        const resp = await fetch(URL);
+        const data = await resp.json();
+
+        console.log(data);
         
     }
+
+    getGifs();
 
     return (
         <div>
