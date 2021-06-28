a52df9f7f7d93b181a080935f0183ea01d6ab3af
jrpsdev <jrpsdev@gmail.com>
2021-06-28 18:04:31 -0500

    Se agrego el useEffect GifGrid

--- src/componets/GifGrid.js
+++ src/componets/GifGrid.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const GifGrid = ( { category } ) => {
+
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        getGifs();
+    }, []);
 
     const getGifs = async () => {
         const URL = 'https://api.giphy.com/v1/gifs/search?q=Samurai+X&limit=5&api_key=xihgKvxIn5LiQz36dS5z6Fn3p3yXMPKM';
         const resp = await fetch(URL);
-        const data = await resp.json();
+        const { data } = await resp.json();
 
-        console.log(data);
+        const gifs = data.map( img => {
+            return {
+                id: img.id,
+                tittle: img.tittle,
+                url: img.images?.downsized_medium.url
+            }
+        });
+
+        console.log(gifs);
         
     }
 
-    getGifs();
-
     return (
         <div>
-            <h3>{ category }</h3>
+            <h3>{ counter }</h3>
+            <button onClick = { () => setCounter( counter + 1) }>1</button>
         </div>
     )
 }
